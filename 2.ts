b4aeca58f2552782eb68c28faf86a542d30bc77d
Alexander Bespalov <alexander.bespalov@clockwise.software>
2021-10-19 15:22:15 +0300

    added 1-2 lessons

---
+++ 2.ts
@@ -0,0 +1,17 @@
+const today = new Date();
+today.getMinutes(); //определяет тип и понимает какие методы есть
+
+class Color {}
+const red = new Color();
+
+let applesCount: number = 5;
+let text: string = 'Text string';
+let isAdded: boolean = false;
+let nothing: null = null;
+
+// built in objects
+let now: Date = new Date();
+
+// array
+// array of strings
+let colors: string[] = ['red', 'green'];
